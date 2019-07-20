55ba322bca4d23ec8c9be1178e0a1325c5672956
hafijur <hafijur2584@gmail.com>
2019-07-21 03:01:48 +0600

    homepage product dynamic

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Product;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::inRandomOrder()->take(8)->get();
+        return view('home')->with('products',$products);
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -101,139 +101,26 @@
 
                 <div id="code_prod2">
                     <div class="row">
-                        <div class="col-md-3">
-                            <figure class="card card-product">
-                                <div class="img-wrap">
-                                    <img src="{{ asset('user/images/items/3.jpg') }}">
-                                    <a class="btn-overlay" href="#"><i class="fa fa-search-plus"></i> Quick view</a>
-                                </div>
-                                <figcaption class="info-wrap">
-                                    <a href="#" class="title">Good item name</a>
-                                    <div class="action-wrap">
-                                        <a href="#" class="btn btn-warning btn-sm float-right"> <i class="fa fa-shopping-cart"></i>  Add To Cart </a>
-                                        <div class="price-wrap h5">
-                                            <span class="price-new">$1280</span>
-                                            <del class="price-old">$1980</del>
-                                        </div> <!-- price-wrap.// -->
-                                    </div> <!-- action-wrap -->
-                                </figcaption>
-                            </figure> <!-- card // -->
-                        </div> <!-- col // -->
-                        <div class="col-md-3">
-                            <figure class="card card-product">
-                                <div class="img-wrap"> <img src="{{ asset('user/images/items/4.jpg') }}">
-                                    <a class="btn-overlay" href="#"><i class="fa fa-search-plus"></i> Quick view</a>
-                                </div>
-                                <figcaption class="info-wrap">
-                                    <a href="#" class="title">The name of product</a>
-                                    <div class="action-wrap">
-                                        <a href="#" class="btn btn-warning btn-sm float-right"> Add To Cart </a>
-                                        <div class="price-wrap h5">
-                                            <span class="price-new">$280</span>
-                                        </div> <!-- price-wrap.// -->
-                                    </div> <!-- action-wrap -->
-                                </figcaption>
-                            </figure> <!-- card // -->
-                        </div> <!-- col // -->
-                        <div class="col-md-3">
-                            <figure class="card card-product">
-                                <div class="img-wrap"> <img src="{{ asset('user/images/items/5.jpg') }}">
-                                    <a class="btn-overlay" href="#"><i class="fa fa-search-plus"></i> Quick view</a>
-                                </div>
-                                <figcaption class="info-wrap">
-                                    <a href="#" class="title">Name of product</a>
-                                    <div class="action-wrap">
-                                        <a href="#" class="btn btn-warning btn-sm float-right"> Add To Cart </a>
-                                        <div class="price-wrap h5">
-                                            <span class="price-new">$280</span>
-                                        </div> <!-- price-wrap.// -->
-                                    </div> <!-- action-wrap -->
-                                </figcaption>
-                            </figure> <!-- card // -->
-                        </div> <!-- col // -->
-                        <div class="col-md-3">
-                            <figure class="card card-product">
-                                <div class="img-wrap"> <img src="{{ asset('user/images/items/6.jpg') }}">
-                                    <a class="btn-overlay" href="#"><i class="fa fa-search-plus"></i> Quick view</a>
-                                </div>
-                                <figcaption class="info-wrap">
-                                    <a href="#" class="title">The name of product</a>
-                                    <div class="action-wrap">
-                                        <a href="#" class="btn btn-warning btn-sm float-right"> Add To Cart </a>
-                                        <div class="price-wrap h5">
-                                            <span class="price-new">$280</span>
-                                        </div> <!-- price-wrap.// -->
-                                    </div> <!-- action-wrap -->
-                                </figcaption>
-                            </figure> <!-- card // -->
-                        </div> <!-- col // -->
-
-                        <div class="col-md-3">
-                            <figure class="card card-product">
-                                <div class="img-wrap">
-                                    <img src="{{ asset('user/images/items/3.jpg') }}">
-                                    <a class="btn-overlay" href="#"><i class="fa fa-search-plus"></i> Quick view</a>
-                                </div>
-                                <figcaption class="info-wrap">
-                                    <a href="#" class="title">Good item name</a>
-                                    <div class="action-wrap">
-                                        <a href="#" class="btn btn-warning btn-sm float-right"> Add To Cart </a>
-                                        <div class="price-wrap h5">
-                                            <span class="price-new">$1280</span>
-                                            <del class="price-old">$1980</del>
-                                        </div> <!-- price-wrap.// -->
-                                    </div> <!-- action-wrap -->
-                                </figcaption>
-                            </figure> <!-- card // -->
-                        </div> <!-- col // -->
-                        <div class="col-md-3">
-                            <figure class="card card-product">
-                                <div class="img-wrap"> <img src="{{ asset('user/images/items/4.jpg') }}">
-                                    <a class="btn-overlay" href="#"><i class="fa fa-search-plus"></i> Quick view</a>
-                                </div>
-                                <figcaption class="info-wrap">
-                                    <a href="#" class="title">The name of product</a>
-                                    <div class="action-wrap">
-                                        <a href="#" class="btn btn-warning btn-sm float-right"> Add To Cart </a>
-                                        <div class="price-wrap h5">
-                                            <span class="price-new">$280</span>
-                                        </div> <!-- price-wrap.// -->
-                                    </div> <!-- action-wrap -->
-                                </figcaption>
-                            </figure> <!-- card // -->
-                        </div> <!-- col // -->
-                        <div class="col-md-3">
-                            <figure class="card card-product">
-                                <div class="img-wrap"> <img src="{{ asset('user/images/items/5.jpg') }}">
-                                    <a class="btn-overlay" href="#"><i class="fa fa-search-plus"></i> Quick view</a>
-                                </div>
-                                <figcaption class="info-wrap">
-                                    <a href="#" class="title">Name of product</a>
-                                    <div class="action-wrap">
-                                        <a href="#" class="btn btn-warning btn-sm float-right"> Add to Cart </a>
-                                        <div class="price-wrap h5">
-                                            <span class="price-new">$280</span>
-                                        </div> <!-- price-wrap.// -->
-                                    </div> <!-- action-wrap -->
-                                </figcaption>
-                            </figure> <!-- card // -->
-                        </div> <!-- col // -->
-                        <div class="col-md-3">
-                            <figure class="card card-product">
-                                <div class="img-wrap"> <img src="{{ asset('user/images/items/6.jpg') }}">
-                                    <a class="btn-overlay" href="#"><i class="fa fa-search-plus"></i> Quick view</a>
-                                </div>
-                                <figcaption class="info-wrap">
-                                    <a href="#" class="title">The name of product</a>
-                                    <div class="action-wrap">
-                                        <a href="#" class="btn btn-warning btn-sm float-right"> Add To Cart </a>
-                                        <div class="price-wrap h5">
-                                            <span class="price-new">$280</span>
-                                        </div> <!-- price-wrap.// -->
-                                    </div> <!-- action-wrap -->
-                                </figcaption>
-                            </figure> <!-- card // -->
-                        </div> <!-- col // -->
+                        @foreach($products as $product)
+                            <div class="col-md-3">
+                                <figure class="card card-product">
+                                    <div class="img-wrap">
+                                        <img src="{{ asset('user/images/items/4.jpg') }}">
+                                        <a class="btn-overlay" href="#"><i class="fa fa-search-plus"></i> Quick view</a>
+                                    </div>
+                                    <figcaption class="info-wrap">
+                                        <a href="#" class="title">{{ $product->name }}</a>
+                                        <div class="action-wrap">
+                                            <a href="#" class="btn btn-warning btn-sm float-right"> <i class="fa fa-shopping-cart"></i>  Add To Cart </a>
+                                            <div class="price-wrap h5">
+                                                <span class="price-new">${{ $product->price }}</span>
+                                                {{--<del class="price-old">$1980</del>--}}
+                                            </div> <!-- price-wrap.// -->
+                                        </div> <!-- action-wrap -->
+                                    </figcaption>
+                                </figure> <!-- card // -->
+                            </div> <!-- col // -->
+                        @endforeach
                     </div> <!-- row.// -->
                 </div>
             </div>
