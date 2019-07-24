db788e744ea95311b4c16a91230f582614397807
hafijur <hafijur2584@gmail.com>
2019-07-24 18:09:38 +0600

    login page update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,127 @@
-@extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="Bootstrap-ecommerce by Vosidiy">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <title>E-commerce | Login </title>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('user/images/favicon.ico') }}">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    <!-- jQuery -->
+    <script src="{{ asset('user/js/jquery-2.0.0.min.js') }}" type="text/javascript"></script>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+    <!-- Bootstrap4 files-->
+    <script src="{{ asset('user/js/bootstrap.bundle.min.js') }}" type="text/javascript"></script>
+    <link href="{{ asset('user/css/bootstrap.css') }}" rel="stylesheet" type="text/css"/>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+    <!-- Font awesome 5 -->
+    <link href="{{ asset('user/fonts/fontawesome/css/fontawesome-all.min.css') }}" type="text/css" rel="stylesheet">
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+    <!-- plugin: owl carousel  -->
+    <link href="{{ asset('user/plugins/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('user/plugins/owlcarousel/assets/owl.theme.default.css') }}" rel="stylesheet">
+    <script src="{{ asset('user/plugins/owlcarousel/owl.carousel.min.js') }}"></script>
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+    <!-- custom style -->
+    <link href="{{ asset('user/css/ui.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('user/css/responsive.css') }}" rel="stylesheet" media="only screen and (max-width: 1200px)"/>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+    <!-- custom javascript -->
+    <script src="{{ asset('user/js/script.js') }}" type="text/javascript"></script>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
+    <script type="text/javascript">
+        /// some script
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+        // jquery ready start
+        $(document).ready(function () {
+            // jQuery code
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+        });
+        // jquery end
+    </script>
+
+</head>
+<body>
+<header class="section-header">
+
+
+</header> <!-- section-header.// -->
+
+
+<!-- ========================= SECTION CONTENT ========================= -->
+
+<section class="section-content bg padding-y-sm">
+    <div class="container">
+        <nav class="mb-">
+
+        </nav>
+        <div class="row">
+            <div class="col-xl-12 col-md-9 col-sm-12">
+                <div class="row">
+
+                    <aside class="col-sm-3">
+
+                    </aside>
+
+                    <!-- PRODUCT DETAIL -->
+                    <aside class="col-sm-6">
+
+                        <div id="code_register_1">
+                            <div class="card">
+                                <header class="card-header">
+                                    <a href="/" class="float-right btn btn-outline-primary mt-1">Home </a>
+                                    <h4 class="card-title mt-2">Sign In</h4>
+                                </header>
+                                <article class="card-body">
+                                    <form action="{{ route('login') }}" method="post">
+                                        @csrf
+                                        <div class="form-group">
+                                            <label>Email</label>
+                                            <input id="email" name="email" value="{{ old('email') }}" type="email" class="form-control">
+                                            @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div> <!-- form-group end.// -->
+                                        <div class="form-group">
+                                            <label>Password</label>
+                                            <input class="form-control" type="password" name="password" id="password" >
+                                            @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div> <!-- form-group end.// -->
+                                        <div class="form-group">
+                                            <button type="submit" class="btn btn-primary btn-block"> Login</button>
+                                        </div> <!-- form-group// -->
+                                        <small class="text-muted">By clicking the 'Sign Up' button, you confirm that you
+                                            accept our <br> Terms of use and Privacy Policy.
+                                        </small>
+                                    </form>
+                                </article>
+                            </div> <!-- card.// -->
+
+                        </div> <!-- code-wrap.// -->
+                    </aside>
+                    <!-- card.// -->
+
+                    <!-- PRODUCT DETAIL .// -->
+
                 </div>
+                <!-- col // -->
+
+
             </div>
+            <!-- row.// -->
+
         </div>
     </div>
-</div>
-@endsection
+        <!-- container // -->
+</section>
+<!-- ========================= SECTION CONTENT .END// ========================= -->
+</body>
